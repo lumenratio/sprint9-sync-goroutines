c7ee7374d27a0900eae541b7d025ed55ab6282e6
Mikhail Pluzhnikov <flamerat@yandex.ru>
2025-09-14 18:58:25 +0300

    S9SYNC: First commit * Only channels * No used mutex * Add unit tests * Modify core functions: add error return

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -10,31 +13,131 @@ const (
 )
 
 // generateRandomElements generates random elements.
-func generateRandomElements(size int) []int {
+func generateRandomElements(size int) ([]int, error) {
 	// ваш код здесь
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	if size <= 0 {
+		return nil, fmt.Errorf("given size, zero or less")
+	}
+
+	numbers := make([]int, 0, size)
+	for i := 0; i < size; i++ {
+		numbers = append(numbers, int(rnd.Int63()))
+	}
+	return numbers, nil
 }
 
 // maximum returns the maximum number of elements.
-func maximum(data []int) int {
+func maximum(data []int) (int, error) {
 	// ваш код здесь
+	if len(data) == 0 {
+		return 0, fmt.Errorf("len of given slice is zero")
+	}
+	if len(data) == 1 {
+		return data[0], nil
+	}
+
+	// да, решил использовать сортировку sort.Ints(), а не простой перебор элементов с условием max < v
+	// а потом суммарно 8 часов идиотии, что бы понять, почему нижележайший массив меняется и в maxChunks передается отсортированный массив (по началу показалось, что вообще другой)
+	//sort.Ints(data)
+	//max := data[len(data)-1]
+	// поэтому for range data ...
+	// и минус 8 часов и массу нервов
+	max := data[0]
+	for _, v := range data {
+		if max < v {
+			max = v
+		}
+	}
+	return max, nil
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
-func maxChunks(data []int) int {
+func maxChunks(data []int) (int, error) {
 	// ваш код здесь
+	// check data parameters
+	if len(data) == 0 {
+		return 0, fmt.Errorf("len of given slice is zero")
+	}
+
+	if len(data) == 1 {
+		return data[0], nil
+	}
+
+	if CHUNKS <= 0 {
+		return 0, fmt.Errorf("given number of chunks is zero or less")
+	}
+
+	// if len(data) < 8, then chunks count must be lower than default CHUNKS
+	numChunks := CHUNKS
+	if len(data) < 8 {
+		numChunks = len(data)
+	}
+
+	// done channel
+	done := make(chan struct{})
+	defer close(done)
+	// channel for goroutines, that send result
+	getMaxNum := make(chan int, numChunks)
+	// slice for results from goroutines
+	var chunksResults []int
+	go func() {
+		for v := range getMaxNum {
+			chunksResults = append(chunksResults, v)
+		}
+		done <- struct{}{}
+	}()
+
+	var wg sync.WaitGroup
+	// ok, lets chunk data and run goroutines
+	for i := 0; i < numChunks; i++ {
+		start := i * len(data) / numChunks
+		end := ((i + 1) * len(data)) / numChunks
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			chunk := data[start:end]
+			max, _ := maximum(chunk)
+			getMaxNum <- max
+		}()
+	}
+	wg.Wait()
+	//all gouroutines did their task, close the channel
+	close(getMaxNum)
+	//wait when goroutine that collecting results, complete
+	<-done
+	return maximum(chunksResults)
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
+	rndNumbers, err := generateRandomElements(SIZE)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
+	start := time.Now()
+	max, err := maximum(rndNumbers)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	elapsed := time.Since(start)
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
 	// ваш код здесь
+	start = time.Now()
+	max, err = maxChunks(rndNumbers)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	elapsed = time.Since(start)
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,99 @@
 package main
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
 // Пишите тесты в этом файле
+func Test_generateRandomElements(t *testing.T) {
+	tests := []struct {
+		name string
+		size int
+		want int
+		err  bool
+	}{
+		{name: "Zero size", size: 0, want: 0, err: true},
+		{name: "Negative size", size: -1, want: 0, err: true},
+		{name: "Positive size: 1", size: 1, want: 1, err: false},
+		{name: "Positive size: big num", size: 10_000, want: 10_000, err: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := generateRandomElements(tt.size)
+			if tt.err {
+				assert.Len(t, got, tt.want)
+				require.Error(t, gotErr)
+				return
+			}
+			require.NoError(t, gotErr, "got an error, but should not")
+			assert.Len(t, got, tt.want)
+		})
+	}
+}
+
+func Test_maximum(t *testing.T) {
+	tests := []struct {
+		name string
+		data []int
+		want int
+		err  bool
+	}{
+		{name: "Zero len of slice", data: []int{}, want: 0, err: true},
+		{name: "Send nil slice", data: nil, want: 0, err: true},
+		{name: "One element in slice: zero", data: []int{0}, want: 0, err: false},
+		{name: "One element in slice: negative number", data: []int{-1}, want: -1, err: false},
+		{name: "One element in slice: positive number", data: []int{1}, want: 1, err: false},
+		{name: "Small slice: negative numbers", data: []int{-2, -5, -3, -1, -10, -8}, want: -1, err: false},
+		{name: "Small slice: positive numbers", data: []int{2, 5, 3, 1, 10, 8}, want: 10, err: false},
+		{name: "Small slice: mixed numbers", data: []int{2, 5, -4, 3, -6, 1, 10, -5, 8}, want: 10, err: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := maximum(tt.data)
+			if tt.err {
+				assert.Equal(t, tt.want, got)
+				require.Error(t, gotErr)
+				return
+			}
+			assert.Equal(t, tt.want, got)
+			require.NoError(t, gotErr, "got an error, but should not")
+		})
+	}
+}
+
+func Test_maxChunks(t *testing.T) {
+	tests := []struct {
+		name string
+		data []int
+		want int
+		err  bool
+	}{
+		{name: "Zero len of slice", data: []int{}, want: 0, err: true},
+		{name: "Send nil slice", data: nil, want: 0, err: true},
+		{name: "One element in slice: zero", data: []int{0}, want: 0, err: false},
+		{name: "One element in slice: negative number", data: []int{-1}, want: -1, err: false},
+		{name: "One element in slice: positive number", data: []int{1}, want: 1, err: false},
+		{name: "Small slice: negative numbers", data: []int{-2, -5, -3, -1, -10, -8}, want: -1, err: false},
+		{name: "Small slice: positive numbers", data: []int{2, 5, 3, 1, 10, 8}, want: 10, err: false},
+		{name: "Small slice: mixed numbers, 9 values", data: []int{2, 5, -4, 3, -6, 1, 10, -5, 8}, want: 10, err: false},
+		{name: "Big slice: mixed numbers, 33 values", data: []int{188, 99, 25, 49, 144, 89, 75, 45, 75, 35, 133, 190,
+			22, 156, 99, 185, 105, 183, 145, 105, 112, 180, 69, 54,
+			104, 87, 155, 94, 45, 89, 142, 146, 133},
+			want: 190, err: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := maxChunks(tt.data)
+			if tt.err {
+				assert.Equal(t, tt.want, got)
+				require.Error(t, gotErr)
+				return
+			}
+			assert.Equal(t, tt.want, got)
+			require.NoError(t, gotErr, "got an error, but should not")
+		})
+	}
+}
